ea80b95135596a4cf8391b7b1663832401ecf42b
Soft-UI <vuffidepah-8833@yopmail.com>
2016-01-08 19:31:22 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,14 +2,13 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := GoogleKoreanKeyboard
+LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+LOCAL_MODULE := GoogleKoreanKeyboard
 LOCAL_SRC_FILES := GoogleKoreanKeyboard.apk
 LOCAL_SRC_FILES := lib/arm/liben_data_bundle_preload.so
 LOCAL_SRC_FILES := lib/arm/libgnustl_shared.so
 LOCAL_SRC_FILES := lib/arm/libjni_hmm_shared_engine.so
 LOCAL_SRC_FILES := libkorean_data_bundle_preload.so
-LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
-
